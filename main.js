345e4a7863baf005b3a002516dfd7b984a30b922
advn1 <135426318+advn1@users.noreply.github.com>
2024-07-22 20:27:33 +0500

    add showing on text click. rework logic (optimized also)

--- main.js
+++ main.js
@@ -1,15 +1,31 @@
-const buttons = document.getElementsByClassName("btn")
+window.onload = () => {
+    const buttons = document.getElementsByClassName("btn")
+    const texts = document.getElementsByClassName("q-text")
 
-function handleExpand(btn) {
-    const answer = document.querySelector('.' + btn.alt);
-    answer.classList.toggle('show');
-
-    const imageExpand = btn.classList.toggle('plus')
     const baseUrl = "./faq-accordion-main/assets/images/"
+    let btnExpand = null;
 
-    imageExpand ? btn.src = baseUrl + "icon-plus.svg" : btn.src = baseUrl + "icon-minus.svg"
+    function toggleBtn(element, isText = true) {
+        if (isText) {
+            let allBtnExpands = Array.from(document.getElementsByTagName("img"))
+            btnExpand = allBtnExpands.find(btn => btn.alt === element.ariaLabel)
+        } else {
+            btnExpand = element
+        }
+
+        const imageExpand = btnExpand.classList.toggle('plus')
+
+        imageExpand ? btnExpand.src = baseUrl + "icon-plus.svg" : btnExpand.src = baseUrl + "icon-minus.svg"
+        const answer_text = document.querySelector("." + btnExpand.alt)
+        answer_text.classList.toggle('show')
+    }
+
+    for (let btn of buttons) {
+        btn.addEventListener("click", () => toggleBtn(btn, isText = false))
+    }
+
+    for (let text of texts) {
+        text.addEventListener("click", () => toggleBtn(text))
+    }
 }
 
-for (let btn of buttons) {
-    btn.addEventListener("click", () => handleExpand(btn))
-}
